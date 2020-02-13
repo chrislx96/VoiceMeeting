a627709887b99cc232ab35d8d14ca4d9a8a6482e
Yakun95 <yangyakun95@gmail.com>
2020-02-13 23:59:45 +1100

    rm yakun directory

--- yakun/mysite/wave_factory/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from .model.speakerUtil import Predictor
-
-
-class UploadwaveConfig(AppConfig):
-    name = 'wave_factory'
-
-    predictor = Predictor()
-    predictor.load_model()
